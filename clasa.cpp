81df0baf563f1bbe5ec5381fec57e58fcea6f2e6
mariuscolac261 <marius-daniel.colac@s.unibuc.ro>
2022-11-07 16:47:29 +0200

    Update

--- clasa.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by colac on 10/28/2022.
-//
-
-#include "clasa.h"
-
-clasa::clasa(const std::string &name, int price, const std::string brand) : name(name), price(price), brand(brand) {}
