54784fda902b6d547d0c4c26669067afe6f6a72f
mariuscolac261 <marius-daniel.colac@s.unibuc.ro>
2023-01-05 12:13:01 +0200

    tema2.0

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include "Programator.h"
 int main()
 {
-    Programator p1{"3", {Contabil{"Adrian", 20, 80}.clone()}
-    };
-    std::cout <<  p1.salariuF();
-
+ Programator p1{"3", {Contabil{"Adrian", 20, 80}.clone()}
+};
+ std::cout <<  p1.salariuF();
 }
